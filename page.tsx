bbafe1c9cbf3fb57faa7852e550d2e4b905feff7
Shakti Sidhu <sidshakti02@gmail.com>
2026-06-21 21:24:57 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-white text-8xl">
+        SHAKTI TEST
+      </h1>
+    </main>
+  );
+}
